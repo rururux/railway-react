8f17e84a8a20ef842f54a7ef07a2d5437e4a351f
rururux <rururux@gmail.com>
2025-05-30 22:57:24 +0900

    スレッド作成ページ用のリンクを追加

--- mission-1-bbs/src/App.tsx
+++ mission-1-bbs/src/App.tsx
@@ -1,13 +1,15 @@
 import { Layout } from "./components/layout"
 import { Route, Routes } from "./components/router"
+import CreateThreadPage from "./pages/createThread"
 import IndexPage from "./pages/index"
 
 export default function BBS() {
   return (
-    <Layout>
-      <Routes>
+    <Routes>
+      <Layout>
         <Route path="/" element={<IndexPage />} />
-      </Routes>
-    </Layout>
+        <Route path="/threads/new" element={<CreateThreadPage />} />
+      </Layout>
+    </Routes>
   )
 }
--- mission-1-bbs/src/components/layout/index.tsx
+++ mission-1-bbs/src/components/layout/index.tsx
@@ -1,11 +1,13 @@
 import type { PropsWithChildren } from "react"
 import styles from "./styles.module.css"
+import { Link } from "../router"
 
 export function Layout({ children }: PropsWithChildren) {
   return (
     <>
       <header className={styles.header}>
         <h1 className={styles.headerTitle}>掲示板</h1>
+        <Link className={styles.createThreadLink} to="/threads/new">スレッドをたてる</Link>
       </header>
       <main className={styles.main}>
         {children}
