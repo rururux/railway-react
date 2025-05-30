207542c4291243cb3e9668561a67b9cfcd8a3402
rururux <rururux@gmail.com>
2025-05-30 22:23:29 +0900

    スタイルを近付ける

--- mission-1-bbs/src/pages/index/index.tsx
+++ mission-1-bbs/src/pages/index/index.tsx
@@ -2,17 +2,18 @@ import { Link } from "../../components/router"
 import { Thread } from "../../components/thread"
 import { ThreadList } from "../../components/threadList"
 import { useBBSApi } from "../../hooks/useBBSApi"
+import styles from "./styles.module.css"
 
 export default function IndexPage() {
   const { data: threads, error, isLoading } = useBBSApi("/threads")
 
   return (
     <div>
-      <h2>新着スレッド</h2>
+      <h2 className={styles.pageTitle}>新着スレッド</h2>
       {threads !== undefined && (
         <ThreadList>
           {threads.map(thread => (
-            <Link to={thread.id}>
+            <Link to={thread.id} key={thread.id}>
               <Thread>{thread.title}</Thread>
             </Link>
           ))}
