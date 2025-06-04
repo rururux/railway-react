f4a0877d614ec9683a3ccd5237ca7d5ae88249fc
rururux <rururux@gmail.com>
2025-06-04 23:00:09 +0900

    SSR 実装 & station 4 の用意

--- mission-4-ssr/server.ts
+++ mission-4-ssr/server.ts
@@ -1,7 +1,61 @@
-import express from "express"
+// https://ja.vite.dev/guide/ssr.html
 
-const app = express()
+import fs from "node:fs/promises"
+import express, { type Response } from "express"
+import type { ViteDevServer } from "vite"
 
-app.use(express.static("./mission-4-ssr/dist"))
+const isProduction = process.env.NODE_ENV === "production"
 
-app.listen(3000)
+;(async () => {
+  const app = express()
+  let vite: ViteDevServer
+
+  if (!isProduction) {
+    const { createServer: createViteServer } = await import("vite")
+    vite = await createViteServer({
+      root: "./mission-4-ssr",
+      configFile: "vite.config.ts",
+      server: { middlewareMode: true },
+      appType: "custom"
+    })
+
+    app.use(vite.middlewares)
+  } else {
+    app.use(express.static("./mission-4-ssr/dist/client"))
+  }
+
+  app.use('*all', async (req, res, next) => {
+    const url = req.originalUrl
+
+    try {
+      let template: string
+      let render: (res: Response) => Promise<void>
+
+      if (!isProduction) {
+        template = await fs.readFile("./mission-4-ssr/index.html", "utf-8")
+        template = await vite.transformIndexHtml(url, template)
+
+        render = await vite.ssrLoadModule("./mission-4-ssr/src/entry-server.tsx").then(r => r.render)
+      } else {
+        template = await fs.readFile("./mission-4-ssr/dist/client/index.html", "utf-8")
+        // @ts-expect-error あるはず
+        render = await import("./mission-4-ssr/dist/server/entry-server.js").then(r => r.render)
+      }
+
+      const htmlParts = template.split("<!--ssr-outlet-->")
+
+      res.status(200).set({ "Content-Type": "text/html" })
+      res.write(htmlParts[0])
+      await render(res)
+      res.end(htmlParts[1])
+    } catch (e) {
+      if (e instanceof Error && vite !== undefined) {
+        vite.ssrFixStacktrace(e)
+      }
+
+      next(e)
+    }
+  })
+
+  app.listen(3000)
+})()
--- mission-4-ssr/src/App.tsx
+++ mission-4-ssr/src/App.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
 export default function App() {
+  const [ count, setCount ] = useState(0)
+
   return (
     <div className="min-h-dvh grid place-items-center" style={{ backgroundImage: "linear-gradient(220.55deg, #565656 0%, #181818 100%)" }}>
       <div className="w-md p-8 rounded-xl bg-white/70 backdrop-blur-md grid justify-items-center gap-2">
@@ -8,6 +12,13 @@ export default function App() {
         <div className="text-gray-800 flex flex-col items-center py-6">
           <p>日本生まれ日本育ちの日系ブラジル人です。</p>
           <p>フロントエンドが好きです。</p>
+        </div>
+        <hr className="border-t-2 border-t-gray-400 w-full" />
+        <div className="flex items-center gap-4 pt-4">
+          <p>
+            count: <output className="font-bold">{count}</output>
+          </p>
+          <button className="text-white bg-blue-500 px-3 py-2 rounded-sm" onClick={() => setCount(c => c + 1)}>Count Up</button>
         </div>
       </div>
     </div>
