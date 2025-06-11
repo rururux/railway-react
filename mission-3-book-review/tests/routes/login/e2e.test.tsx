982153769573da35b14eb5f7c5d8d08ff4c4a769
rururux <rururux@gmail.com>
2025-06-11 19:48:24 +0900

    テストの更新

--- mission-3-book-review/tests/routes/login/e2e.test.tsx
+++ mission-3-book-review/tests/routes/login/e2e.test.tsx
@@ -1,10 +1,16 @@
 import { expect, test } from "vitest"
 import { render } from "vitest-browser-react"
 import LoginPage from "../../../app/routes/login"
+import { createRoutesStub } from "react-router"
+import { LoginSchema } from "../../../app/schemas"
+import { ZodError } from "zod/v4"
 
 test("required", async () => {
+  const Stab = createRoutesStub([
+    { path: "/login", Component: LoginPage }
+  ])
   const screen = render(
-    <LoginPage />
+    <Stab initialEntries={["/login"]} />
   )
 
   const emailInput = screen.getByLabelText("メール")
@@ -17,11 +23,37 @@ test("required", async () => {
 
   // submit 失敗
   await expect.element(emailInput).toHaveFocus()
+  await expect.element(screen.getByText("メールアドレスを入力してください")).toBeInTheDocument()
+  await expect.element(screen.getByText("最低7文字以上は入力してください")).toBeInTheDocument()
 })
 
 test("validation", async () => {
+  const Stab = createRoutesStub([
+    {
+      path: "/login",
+      Component: LoginPage,
+      async action({ request }) {
+        try {
+          const requestData = await request.json()
+
+          LoginSchema.parse(requestData)
+        } catch (e) {
+          if (e instanceof ZodError) {
+            return e
+              .issues
+              .reduce<{ errors: Record<string, string>[] }>(
+                (prv, cur) => (cur.path.length > 0)? { errors: [ ...prv.errors, { [cur.path[0]]: cur.message } ] } : prv,
+                { errors: [] }
+              )
+          } else {
+            return { errors: [ { root: "unknown server error" } ] }
+          }
+        }
+      }
+    }
+  ])
   const screen = render(
-    <LoginPage />
+    <Stab initialEntries={["/login"]} />
   )
 
   const emailInput = screen.getByLabelText("メール")
@@ -36,13 +68,13 @@ test("validation", async () => {
   await submitButton.click()
 
   // submit 失敗
-  await expect.element(emailInput).toHaveFocus()
+  await expect.element(screen.getByText("正しいメールアドレスを入力してください")).toBeInTheDocument()
 
   await emailInput.fill("validmail@example.com")
   await submitButton.click()
 
   // submit 失敗
-  await expect.element(passwordInput).toHaveFocus()
+  await expect.element(screen.getByText("最低7文字以上は入力してください")).toBeInTheDocument()
 
   await passwordInput.fill("sample-password")
   await submitButton.click()
--- mission-3-book-review/tests/routes/login/unit.test.tsx
+++ mission-3-book-review/tests/routes/login/unit.test.tsx
@@ -1,10 +1,14 @@
 import { expect, test } from "vitest"
 import { render } from "vitest-browser-react"
 import LoginPage from "../../../app/routes/login"
+import { createRoutesStub } from "react-router"
 
 test("render", async () => {
+  const Stab = createRoutesStub([
+    { path: "/login", Component: LoginPage }
+  ])
   const screen = render(
-    <LoginPage />
+    <Stab initialEntries={["/login"]} />
   )
 
   const emailInput = screen.getByLabelText("メール")
